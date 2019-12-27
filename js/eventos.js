9bbda8808410e805efc6c2b39ba7d097d06f67db
Hugo Gomes <hugogomes@MBP-de-Hugo-2.lan>
2019-12-27 19:37:40 +0000

    Eventos/Auth UI update

--- js/eventos.js
+++ js/eventos.js
@@ -1,9 +1,9 @@
 const gallery = [
-    "../img/galeria/soy_deforestation.jpg",
-    "../img/galeria/jesus.jpg",
-    "../img/galeria/stop_nucler.jpg",
-    "../img/galeria/stop_shell.jpg",
-    "../img/galeria/eiffel_tower.jpg"
+    "img/galeria/soy_deforestation.jpg",
+    "img/galeria/jesus.jpg",
+    "img/galeria/stop_nucler.jpg",
+    "img/galeria/stop_shell.jpg",
+    "img/galeria/eiffel_tower.jpg"
 ]
 const galleryElement = $('#curr-img')
 let currPos = 0;
@@ -14,28 +14,48 @@ const rightBtn = $('#navigate-right')
 const mobileLeftBtn = $('#navigate-left-mobile')
 const mobileRightBtn = $('#navigate-right-mobile')
 
+const toggleFormModal = $("#open-modal")
+const modalBg = $("#modal > #modal-bg")
+
 let navElements = createBottomNav()
 console.log(navElements)
 setNewGalleryImage(galleryElement, currPos)
-setInterval(()=>{
-    navigateRight()
-}, timing)
+let timer = setInterval(navigateRight, timing)
 
 leftBtn.click(function(){
     navigateLeft()
+    timer = resetTimer(timer, timing)
 })
 
 rightBtn.click(function(){
     navigateRight()
+    timer = resetTimer(timer, timing)
 })
 
 mobileLeftBtn.click(function(){
     navigateLeft()
+    timer = resetTimer(timer, timing)
 })
 
 mobileRightBtn.click(function(){
     navigateRight()
+    timer = resetTimer(timer, timing)
 })
+
+toggleFormModal.click(function(){
+    $("#modal").css("width", "100%")
+    $("#form-container").css("transform", "scale(1)")
+})
+
+modalBg.click(function(){
+    $("#modal").css("width", "0%")
+    $("#form-container").css("transform", "scale(0)")
+})
+
+function resetTimer(timer, timing){
+    clearInterval(timer)
+    return setInterval(navigateRight, timing)
+}
 
 function navigateLeft(){
     navElements[currPos].classList.remove("selected")
